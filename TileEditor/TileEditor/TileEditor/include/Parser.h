61790e427fd486a876e53c8a5f39321f1387d1d2
cpasyanos <pasyanos.c@husky.neu.edu>
2018-03-19 16:28:26 -0400

    parsing editor from file implemented but needs testing

--- TileEditor/TileEditor/TileEditor/include/Parser.h
+++ TileEditor/TileEditor/TileEditor/include/Parser.h
@@ -15,9 +15,9 @@ public:
 	}
 
 	void loadLevel(std::string levelFilePath) {
-		std::ifstream levelFile;
-		std::string temp;
-		int tile;
+		std::ifstream levelFile; // open a new ifstream
+		int tile; // temporary hold for date 
+		bool tilesInit, propsInit, flagsInit = false; // bool flags
 
 		// initialize a new level
 		Level* loaded = new Level();
@@ -30,7 +30,49 @@ public:
 			exit(1);
 		}
 		else {
-			
+			// read char by char
+			char c;
+			int index = 0;
+			while (levelFile >> std::skipws >> c) {
+				if (c == 'X') {
+					if (!tilesInit) {
+						tilesInit = true;
+						index = 0;
+					}
+					else if (!propsInit) {
+						propsInit = true;
+						index = 0;
+					}
+					else if (!flagsInit) {
+						flagsInit = true;
+						index = 0;
+					}
+				}
+				else {
+					tile = c - '0';
+					if (!tilesInit) {
+						loaded->setTileAt(index / NUM_COLUMNS, index % NUM_COLUMNS, tile);
+					} 
+					else if (!propsInit) {
+						loaded->setPropAt(index / NUM_COLUMNS, index % NUM_COLUMNS, tile);
+					}
+					else if (!flagsInit) {
+						loaded->setFlagAt(index / NUM_COLUMNS, index % NUM_COLUMNS, tile);
+					}
+					else {
+						// everything has been initialized
+						break;
+					}
+
+					index++;
+				}
+			}
+		}
+
+		// check to make sure everything was initialized
+		if (!(tilesInit && propsInit && flagsInit)) {
+			std::cout << "Malformed file. Could not load" << std::endl;
+			exit(1);
 		}
 	}
 
